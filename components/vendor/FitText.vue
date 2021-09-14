90091a08daa23e58474a649763dd88cf3eb9fda0
Eric Corriel <ecorriel@sva.edu>
2021-09-14 01:20:55 -0400

    fit-text, post-scroll.proof, searching for final image

---
+++ components/vendor/FitText.vue
@@ -0,0 +1,43 @@
+<template>
+  <span class="fit"><slot></slot></span>
+</template>
+
+<script>
+// 21.09.13 - https://codesandbox.io/s/vwjlp5pj5?file=/src/App.vue:260-304
+import fitty from "fitty";
+
+export default {
+  name: "FitText",
+  props: {
+    options: {
+      type: Object,
+      required: false,
+      default() {
+        return {
+          minSize: 16,
+          maxSize: 512,
+          multiLine: true
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      $_fitty: undefined
+    };
+  },
+  destroyed() {
+    this.$_fitty.unsubscribe();
+  },
+  mounted() {
+    this.$_fitty = fitty(this.$el, this.options);
+  }
+};
+</script>
+
+<style scoped>
+.fit {
+  display: inline-block;
+  white-space: nowrap;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,23 +11,27 @@
       </div>
     </div>
     <div id="step2" ref="step2" class="step">
-      <p>Congratulations!</p>
-      <p>You just scrolled through 450 years</p>
+      <FitText>Congratulations!</FitText>
+      <FitText>You just scrolled through</FitText>
+      <FitText>450 years</FitText>
     </div>
     <div id="step3" class="step">
-      <p>That bottle made in 2021 has finally disintegrated:)</p>
+      <FitText>That bottle made in 2021</FitText>
+      <FitText>has finally disintegrated😅</FitText>
     </div>
     <div id="step4" class="step">
-      <p>Oh wait…there's more:(</p>
+      <FitText>Oh wait–there’s a few more…</FitText>
+    </div>
+    <div id="step5" class="step">
       <div class="img-container cover">
-        <img src="/img/20190423-plastic-pollution.jpg" />
+        <img src="/img/landfill.jpg" />
       </div>
     </div>
     <div id="step5" class="step">
-      <p>Shiiiiiiit…</p>
+      <FitText>Shiiiiiit…</FitText>
     </div>
-    <div id="credits">
-      <p>Credits</p>
+    <div id="credits" class="step">
+      <h1 style="text-align: center">Credits TK</h1>
     </div>
   </div>
 </template>
@@ -48,19 +52,25 @@ import { commentaries } from "~/data/commentaries";
 import YearDisintegrated from "~/components/year-disintegrated.vue";
 import VideoContainer from "~/components/video-container.vue";
 import Commentary from "~/components/commentary.vue";
+import FitText from "~/components/vendor/FitText";
+
 export default defineComponent({
   components: {
     YearDisintegrated,
     VideoContainer,
     Commentary,
+    FitText,
   },
   setup() {
     let frame: number = 1;
     let year: number = new Date().getFullYear();
     let previousScrollPosition: number = 0;
+    let stoppingDisintegratedPercentage = 5;
+
     const scrollPosition: Ref<UnwrapRef<number>> = ref(0);
     const currentYear = year;
     const yearsToDisintegrate = 450;
+
     const yearAsInt: Ref<UnwrapRef<number>> = ref(year);
     const disintegrated: Ref<UnwrapRef<number>> = ref(0.0);
     const commentary: Ref<UnwrapRef<string>> = ref("Scroll Down!");
@@ -76,16 +86,17 @@ export default defineComponent({
     function updateBottle(e) {
       scrollPosition.value =
         (window.pageYOffset || document.documentElement.scrollTop) -
-        (document.documentElement.clientTop || 0);
+        (document.documentElement.clientTop || 0) + window.innerHeight;
 
       scrollPosition.value >= previousScrollPosition
         ? (year += 0.25)
         : (year -= 0.25);
-      disintegrated.value = ((year - currentYear) / yearsToDisintegrate) * 100;
+      disintegrated.value = (disintegrated.value < stoppingDisintegratedPercentage)? ((year - currentYear) / yearsToDisintegrate) * 100 : stoppingDisintegratedPercentage;
       yearAsInt.value = parseInt(year);
       commentary.value = commentaries[yearAsInt.value] ?? commentary.value;
-
       previousScrollPosition = scrollPosition.value;
+
+
     }
     onMounted(() => {
       document.addEventListener("scroll", updateBottle);
@@ -95,10 +106,10 @@ export default defineComponent({
       }, 10000);
     });
 
-    watch(disintegrated, (value, oldValue) => {
-      console.log(scrollPosition.value);
-      if (value > 5) {
+    const unwatch = watch(disintegrated, (value, oldValue) => {
+      if (value >= stoppingDisintegratedPercentage) {
         container.value.style.height = scrollPosition.value + "px";
+
       }
     });
 
@@ -112,13 +123,6 @@ export default defineComponent({
     };
   },
 
-  // watch: {
-  //   disintegrated(value, oldValue) {
-  //     if (value > 5) {
-  //       this.$refs.container.style.height = this.scrollPosition + "px";
-  //     }
-  //   },
-  // },
 });
 </script>
 
@@ -135,23 +139,29 @@ export default defineComponent({
   padding: 0
 
 .step
-  height: 100vh
+
   background-color: #000000
   position: relative
+  display: flex
+  align-items: center
+  flex-wrap: wrap
+  align-content: center
   .img-container
     &.cover
-      width: 100vh
-      height: 100vh
+      width: 100vw
       img
         object-fit: cover
+        width: 100vw
   &#step1
     z-index: 1
   &#step2
     z-index: 2
   &#step3
     z-index: 3
+    height: 100vh
   &#step4
     z-index: 4
+    height: 100vh
   &#step5
     z-index: 5
   &#credits
