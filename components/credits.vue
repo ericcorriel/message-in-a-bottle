54e693f4a0bcdca37cf1d94727d9965f772d9671
Eric Corriel <ecorriel@sva.edu>
2021-09-25 13:01:45 -0400

    adding credits

---
+++ components/credits.vue
@@ -0,0 +1,242 @@
+<template>
+  <div class="credits-container">
+    <SpacerQuarterScreen v-if="spacer == 'quarter'"></SpacerQuarterScreen>
+    <SpacerHalfScreen v-if="spacer == 'half'"></SpacerHalfScreen>
+    <h3>Credits</h3>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row up-1">
+      <h4>Concept/Design/Development</h4>
+      <h5>
+        <a href="https://ericcorriel.com" target="_blank">Eric Corriel</a>
+      </h5>
+      <p>
+        Eric is a public installation artist living in New York City where he
+        teaches art and activism at the
+        <a href="https://sva.edu" target="_blank">School of Visual Arts</a>.
+      </p>
+      <p>
+        Sign up for <a href="#">his newsletter</a> or follow him on
+        <a href="#">Instagram</a> • <a href="#">Twitter</a> •
+        <a href="#">Facebook</a>
+      </p>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row">
+      <h4>
+        Awesome institutions without which this would have never been done
+      </h4>
+      <div class="up-2">
+        <div class="cell">
+          <div class="image-container">
+            <a href="https://www.humanimpactsinstitute.org/" target="_blank"
+              ><img src="/img/logos/human-impacts-institute.on-black.png"
+            /></a>
+          </div>
+          <p>
+            I have worked with <a href="https://www.humanimpactsinstitute.org/" target="_blank">Human Impacts Institute</a> since 2018 and this project is a product of their <a href="https://www.humanimpactsinstitute.org/fellowships" target="_blank">Artist Impact Fellowship</a>, which I was fortunate to be a part of in 2021. In short, HII believes the arts play a critical role in advancing society towards a more just, equal, and beautiful future and have been working towards those goals since 2011.
+          </p>
+        </div>
+
+        <div class="cell">
+          <div class="image-container">
+            <a href="http://www.newtowncreekalliance.org/" target="_blank"
+              ><img src="/img/logos/newtown-creek-alliance.png"
+            /></a>
+          </div>
+          <p>
+            In 2021, I was teamed up with <a href="http://www.newtowncreekalliance.org/" target="_blank">Newtown Creek Alliance</a> as part of Human Impact Institute’s Artist Impact Fellowship. Located in Greenpoint, Brooklyn; NCA is passionately engaged in restoring and revitalizing neighborhoods surrounding
+            <a href="https://en.wikipedia.org/wiki/Newtown_Creek" target="_blank">Newtown Creek</a>. They also have an absolutely
+            <a href="http://www.kingslandwildflowers.com/" target="_blank">beautiful rooftop garden</a>, which I highly recommend visiting.
+          </p>
+        </div>
+      </div>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row">
+      <h4>Awesome institutions: Part II</h4>
+      <div class="up-2">
+        <div class="cell">
+          <div class="image-container">
+            <a href="https://www1.nyc.gov/assets/dsny/site/home" target="_blank"
+              ><img src="/img/logos/nyc-dos.png"
+            /></a>
+          </div>
+          <p>
+            <a href="https://www1.nyc.gov/assets/dsny/site/home" target="_blank">NYC’s Department of Sanitation</a> was very gracious in offering me a ride-along during one of their garbage and recycling pickups in Bushwick, Brooklyn–the first time I ever had to be at a 5:30AM meeting!
+          </p>
+        </div>
+
+        <div class="cell">
+          <div class="image-container">
+            <a href="https://www.wm.com/" target="_blank"
+              ><img src="/img/logos/waste-management.png"
+            /></a>
+          </div>
+          <p>
+            I am thankful to <a href="https://www.wm.com/" target="_blank">Waste Management</a> for organizing a visit to their
+            <a href="https://en.wikipedia.org/wiki/Materials_recovery_facility" target="_blank">Materials Recovery Facility (MRF)</a> in Philadelphia, PA. They are working hard to ensure a clean and sustainable future.
+          </p>
+        </div>
+      </div>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row">
+      <h4>Thank you for making your photos free for me to use and adapt</h4>
+      <div class="up-2">
+        <div class="cell">
+          <div class="image-container">
+            <a
+              href="https://www.pexels.com/photo/plastic-bottle-of-detergent-in-studio-5218021/"
+              target="_blank"
+              ><img src="/img/thanks/pexels-anna-shvets-5218021.jpg"
+            /></a>
+          </div>
+          <p>
+            <a
+              href="https://www.pexels.com/photo/plastic-bottle-of-detergent-in-studio-5218021/"
+              target="_blank"
+              >Anne Shvets</a
+            >
+            on <a href="https://pexels.com" target="_blank">Pexels</a>
+          </p>
+        </div>
+
+        <div class="cell">
+          <div class="image-container">
+            <a
+              href="https://flickr.com/photos/cartridgesave/49501486667/in/photolist-2iqh4o4-vcgZR-2iqdk2Y-sTc6W-a3fYvk-jxxrhs-bn7ERc-FVh35y-hfAWK-4ikHe2-7PpNYb-27fsZcS-f5wRJ1-7P11nG-bsNVnm-4ikGAe-9ewq2n-4ikGTe-6eCPfY-27Gsqnz-2kgKYGT-5AXJFY-inzSF-5YX3vJ-25u9SHN-9BPbSr-7cnKXb-xpP15-cAecj-9jAqDL-9jQdhd-4ivLCc-niMCZj-2iqdjWN-9d9Brh-25u9SKG-6qGhF5-9rn82k-9AZoH5-fAP6z3-2iqfVuz-bbvegc-3MePC3-Va4XYG-mKNzbH-6ZrkQa-26MTQqU-oFEsuc-aBmecD-2i2WBbJ"
+              target="_blank"
+              ><img src="/img/thanks/49501486667_cce7e3e6c9_o.jpg"
+            /></a>
+          </div>
+          <p>
+            <a
+              href="https://flickr.com/photos/cartridgesave/49501486667/in/photolist-2iqh4o4-vcgZR-2iqdk2Y-sTc6W-a3fYvk-jxxrhs-bn7ERc-FVh35y-hfAWK-4ikHe2-7PpNYb-27fsZcS-f5wRJ1-7P11nG-bsNVnm-4ikGAe-9ewq2n-4ikGTe-6eCPfY-27Gsqnz-2kgKYGT-5AXJFY-inzSF-5YX3vJ-25u9SHN-9BPbSr-7cnKXb-xpP15-cAecj-9jAqDL-9jQdhd-4ivLCc-niMCZj-2iqdjWN-9d9Brh-25u9SKG-6qGhF5-9rn82k-9AZoH5-fAP6z3-2iqfVuz-bbvegc-3MePC3-Va4XYG-mKNzbH-6ZrkQa-26MTQqU-oFEsuc-aBmecD-2i2WBbJ"
+              target="_blank"
+              >CartridgeSave Images</a
+            >
+            on <a href="https://flickr.com" target="_blank">Flickr</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row">
+      <h4>Open-source software without which this website would have taken ten million times longer to make</h4>
+      <p class="center">
+        <ul>
+          <li><a href="https://vuejs.org" target="_blank">Vue.js</a></li>
+          <li><a href="https://nuxtjs.org" target="_blank">Nuxt.js</a></li>
+          <li><a href="https://webpack.js.org/" target="_blank">Webpack</a></li>
+          <li><a href="https://eslint.org/" target="_blank">ESLint</a></li>
+          <li><a href="https://sass-lang.com/" target="_blank">Sass</a></li>
+          <li><a href="https://github.com/rikschennink/fitty" target="_blank">Fitty</a></li>
+          <li><a href="https://getbootstrap.com" target="_blank">Bootstrap</a></li>
+          <li><a href="https://google.com/fonts">Google Fonts</a></li>
+          <li><a href="https://www.typescriptlang.org/" target="_blank">Typescript</a> (definitely did not make things go quicker;)</li>
+        </ul>
+      </p>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row">
+      <h4>Sources</h4>
+      <p>
+        <ul>
+      <li>National Oceanic and Atmospheric Administration • <a href="https://www.noaa.gov/education/resource-collections/climate/climate-change-impact" target="_blank">Climate Change Impacts</a></li>
+      <li>World Wildlife Foundation • <a href="https://www.wwf.org.au/news/blogs/the-lifecycle-of-plastics" target="_blank">The Lifecycle of Plastics</a></li>
+      <li>Columbia University • <a href="https://news.climate.columbia.edu/2019/12/27/climate-change-impacts-everyone/" target="_blank">State of the Planet</a></li>
+      <li>Colgate University Sustainability Office • <a href="http://blogs.colgate.edu/sustainability/2011/11/10/styrofoam-why-it-is-harmful-alternatives" target="_blank">Styrofoam: Why It Is Harmful, And Alternatives</a></li>
+      <li>Time Magazine • <a href="https://time.com/5824295/climate-change-future-possibilities/" target="_blank">What the World Will Look Like in 2050 If We Don’t Cut Carbon Emissions in Half</a></li>
+      <li>Stacker • <a href="https://stacker.com/stories/2682/how-long-it-takes-50-common-items-decompose" target="_blank">How Long It Takes 50 Common Items To Decompose</a></li>
+      <li>The Awl • <a href="https://www.theawl.com/2012/08/a-timeline-of-future-events/" target="_blank">A Timeline Of Future Events</a></li>
+      <li>Information is Beautiful • <a href="https://www.informationisbeautiful.net/visualizations/timeline-of-the-far-future/" target="_blank">Timeline of the Far Future</a></li>
+    </ul>
+      </p>
+    </div>
+    <SpacerQuarterScreen></SpacerQuarterScreen>
+    <div class="credit-row no-margin-bottom sig center">
+      <p>No plastic bottles were purchased in the making of this film</p>
+      <br />
+      <div><a href="http://ericcorriel.com" target="_blank"><img src="/img/sig.png" ></a></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import SpacerQuarterScreen from "~/components/spacer/quarter-screen.vue";
+import SpacerHalfScreen from "~/components/spacer/half-screen.vue";
+import SpacerFullScreen from "~/components/spacer/full-screen.vue";
+import FitText from "~/components/vendor/FitText.vue";
+export default defineComponent({
+  name: "Credits",
+  components: {
+    SpacerQuarterScreen,
+    SpacerHalfScreen,
+    SpacerFullScreen,
+    FitText,
+  },
+  props: {
+    spacer: { type: String, default: "half" },
+  },
+});
+</script>
+
+<style lang="sass">
+@import "/assets/styles/v1/base"
+@import "/assets/styles/v1/app"
+
+.credits-container
+  background-color: $bg-color
+  position: relative
+  padding: 0 40px
+  h3, h4, h5, h6
+    text-align: center
+    margin: 20px 0
+    line-height: 1.1em
+  h3
+    font-size: 10rem
+  h4
+    font-size: 5.5rem
+  h5
+    font-size: 3.5rem
+  p
+    font-weight: 300
+    font-size: 2em
+    max-width: 60vw
+    margin: 20px auto
+    &.center
+      text-align: center
+  ul, dl
+    padding: 0
+    display: inline-block
+    list-style-type: none
+    margin: 0 auto
+  .center
+    text-align: center
+  .sig
+    padding-bottom: 40px
+    img
+      max-width: 300px
+      margin: 0 auto
+  .up-2
+    display: flex
+    flex-direction: row
+    flex-wrap: nowrap
+    align-items: start
+    justify-content: space-around
+    margin: 40px 10px 20px
+    .cell
+      display: flex
+      flex-direction: column
+      flex-wrap: wrap
+      padding: 0 20px
+      width: 40vw
+      .image-container
+        text-align: center
+        img
+          max-height: 162px
+          border-radius: 8px
+  .no-margin-bottom
+    margin-bottom: 0
+</style>
