e88895c82d99c4957ee3f2bf9a6ed3a3d3e71d04
Eric Corriel <ecorriel@sva.edu>
2021-10-07 01:07:40 -0400

    note about how to transcode to webm

--- data/constants.ts
+++ data/constants.ts
@@ -4,7 +4,7 @@ export const APP = Object.freeze({
   MOBILE_WIDTH: 834,
   MOVIE_MODE: false,
   VIMEO_ID: "607838921",
-  VIDEO_FILENAME: "shatter.07-4K",
+  VIDEO_FILENAME: "shatter.07-4K", // assumes both webm and mp4 exist in static/video • [https://cloudconvert.com/mov-to-webm]
 });
 
 export interface ScrollSpeed {
