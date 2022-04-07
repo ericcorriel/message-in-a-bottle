f7c58376f6bcc05654eccba9c6a4b904fa00fcf5
Eric Corriel <ecorriel@sva.edu>
2022-04-07 01:22:10 -0400

    v3 fight back images

--- components/vertical-image-strip.vue
+++ components/vertical-image-strip.vue
@@ -3,41 +3,41 @@
     <figure>
       <img
         srcset="
-          /img/fight-back/v2/450-years-sticker-on-poland-spring-bottle-portrait.jpg 1024w,
-          /img/fight-back/v2/450-years-sticker-on-poland-spring-bottle.jpg          1800w
+          /img/fight-back/v3/450-years-sticker-on-poland-spring-bottle-portrait.jpg 1024w,
+          /img/fight-back/v3/450-years-sticker-on-poland-spring-bottle.jpg          1800w
         "
         sizes="(max-width: 600px) 1024w, 1800w"
-        src="/img/fight-back/v2/450-years-sticker-on-poland-spring-bottle.jpg"
+        src="/img/fight-back/v3/450-years-sticker-on-poland-spring-bottle.jpg"
         alt="A 1.25 inch sticker on a Poland Spring bottle that says: 'This plastic will exist for at least 450 years. Tag this company on social media and tell them we need better options.''"
       />
     </figure>
     <figure>
       <img
         srcset="
-          /img/fight-back/v2/450-years-sticker-on-tide-bottle-portrait.jpg 1024w,
-          /img/fight-back/v2/450-years-sticker-on-tide-bottle.jpg          1800w
+          /img/fight-back/v3/450-years-sticker-on-tide-bottle-portrait.jpg 1024w,
+          /img/fight-back/v3/450-years-sticker-on-tide-bottle.jpg          1800w
         "
         sizes="(max-width: 600px) 1024w, 1800w"
-        src="/img/fight-back/v2/450-years-sticker-on-tide-bottle.jpg"
+        src="/img/fight-back/v3/450-years-sticker-on-tide-bottle.jpg"
         alt="A 1.25 inch sticker on a Tide bottle that says: 'This plastic will exist for at least 450 years. Tag this company on social media and tell them we need better options.'"
       />
     </figure>
     <figure>
       <img
         srcset="
-          /img/fight-back/v2/450-years-sticker-on-windex-bottle-portrait.jpg 1024w,
-          /img/fight-back/v2/450-years-sticker-on-windex-bottle.jpg          1800w
+          /img/fight-back/v3/450-years-sticker-on-windex-bottle-portrait.jpg 1024w,
+          /img/fight-back/v3/450-years-sticker-on-windex-bottle.jpg          1800w
         "
         sizes="(max-width: 600px) 1024w, 1800w"
-        src="/img/fight-back/v2/450-years-sticker-on-windex-bottle.jpg"
+        src="/img/fight-back/v3/450-years-sticker-on-windex-bottle.jpg"
         alt="A 1.25 inch sticker on a Windex bottle that says: 'This plastic will exist for at least 450 years. Tag this company on social media and tell them we need better options.'"
       />
     </figure>
     <figure>
       <img
         srcset="
-          /img/fight-back/v2/450-YRS-sticker-sheet-portrait.jpg 1024w,
-          /img/fight-back/v2/450-YRS-sticker-sheet.jpg          1800w
+          /img/fight-back/v3/450-YRS-sticker-sheet-portrait.jpg 1024w,
+          /img/fight-back/v3/450-YRS-sticker-sheet.jpg          1800w
         "
         sizes="(max-width: 600px) 1024w, 1800w"
         src="/img/fight-back/v1/450-years-sticker-sheet.png"
